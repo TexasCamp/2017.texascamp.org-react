360184067d175a61948258b7e02d11ec81046594
Chris Martin <chris.martin@fourkitchens.com>
2017-02-17 15:43:01 -0600

    Implement the start page

--- src/components/Frame/Start/index.js
+++ src/components/Frame/Start/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import background from './start-bg.jpg';
+import logo from './logo.svg';
+import logoOutline from './logo-outline.svg';
 
 function Start() {
   return (
     <div id="start">
-      <h1>START</h1>
+      <Background />
+      <Text />
+      <Logo />
+      <LogoOutline />
+    </div>
+  );
+}
+
+function Background() {
+  return (
+    <img src={background} alt="texas camp background" />
+  );
+}
+
+function Text() {
+  return (
+    <h1>A DRUPAL FRONTIER.</h1>
+  );
+}
+
+function Logo() {
+  return (
+    <div id="logo">
+      <img src={logo} alt="texas camp logo" />
+    </div>
+  );
+}
+
+function LogoOutline() {
+  return (
+    <div id="logo-outline">
+      <img src={logoOutline} alt="texas camp logo outline" />
     </div>
   );
 }
