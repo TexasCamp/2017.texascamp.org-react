645b15f052d93b95b9ab09639c269aef5b6745bd
Chris Martin <chris.martin@fourkitchens.com>
2017-02-17 15:01:27 -0600

    Add social share images and tweak the social component

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import twitter from './twitter.svg';
+import facebook from './facebook.svg';
 
 function Social() {
   return (
     <div id="social">
-      Twitter | Facebook | Code of Conduct
+      <Twitter /> | <Facebook /> | <CodeOfConduct />
     </div>
+  );
+}
+
+function Twitter() {
+  return (
+    <a href="/">
+      <img src={twitter} alt="twitter" />
+    </a>
+  );
+}
+
+function Facebook() {
+  return (
+    <a href="/">
+      <img src={facebook} alt="facebook" />
+    </a>
+  );
+}
+
+function CodeOfConduct() {
+  return (
+    <a href="/">
+      Code of Conduct
+    </a>
   );
 }
 
