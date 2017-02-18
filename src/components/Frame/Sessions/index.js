8010cf4ce9e6060f473e417edce4d69c51645f9f
Chris Martin <chris.martin@fourkitchens.com>
2017-02-17 21:09:57 -0600

    Implement Sessions page

--- src/components/Frame/Sessions/index.js
+++ src/components/Frame/Sessions/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Skeleton from '../Skeleton';
+import background from './sessions-bg.jpg';
+import wanted from './wanted-poster.png';
 
 function Sessions() {
   return (
-    <div id="sessions">
-      <h1>STEP RIGHT UP</h1>
-    </div>
+    <Skeleton
+      class="sessions"
+      backgroundSrc={background}
+      backgroundAlt="texas camp sessions background"
+      title="STEP RIGHT UP"
+      body="Blah blah blah needs words."
+      rightElement={
+        <img src={wanted} alt="submit a session" />
+      }
+    />
   );
 }
 
