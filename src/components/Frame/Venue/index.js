90dd51db5c7c14e70113c9e02435adcb24c1b746
Chris Martin <chris.martin@fourkitchens.com>
2017-03-11 18:20:50 -0600

    Remove background from venue page

--- src/components/Frame/Venue/index.js
+++ src/components/Frame/Venue/index.js
@@ -6,8 +6,6 @@ function Venue() {
     <div name="venue" id="venue" className="venue">
       <Skeleton
         class="venue"
-        backgroundSrc="/"
-        backgroundAlt="texas camp venue background"
         title="In a pickle"
         body="How to get to TexasCamp 2017"
         rightElement={
