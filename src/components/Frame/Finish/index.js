ff6c60fd7bdeddd898cf34eb2d8624f4911cec0e
Brandon Williams <brandon@amazeelabs.com>
2017-03-13 05:28:08 -0500

    Use Page component for Finish page

--- src/components/Frame/Finish/index.js
+++ src/components/Frame/Finish/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import Page from '../../Page';
 import background from './finish-bg.jpg';
 
 function Finish() {
   return (
-    <div id="finish">
-      <Background />
-      <Text />
-    </div>
-  );
-}
-
-function Background() {
-  return (
-    <img src={background} alt="texas camp finish background" />
-  );
-}
-
-function Text() {
-  return (
-    <div className="finish__quote"><blockquote className="finish__quote-text">
-      <p>You may all go to hell,<br />and I will go to Texas.</p></blockquote>
-      <p className="finish__quote-author">David Crockett</p></div>
+    <Page name="finish" background={background}>
+      <div className="finish__quote">
+        <blockquote className="finish__quote-text">
+          <p>You may all go to hell,<br />and I will go to Texas.</p>
+        </blockquote>
+        <p className="finish__quote-author">David Crockett</p>
+      </div>
+    </Page>
   );
 }
 
