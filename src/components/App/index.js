add98bcf954741a0d07b6e4e4fd2f53fd834944d
Brandon Williams <brandon@amazeelabs.com>
2017-03-14 15:32:51 -0500

    Add react-router to render either HomeScreen or SubmitSessionScreen

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import CampInfo from '../CampInfo';
 import Menu from '../Menu';
 import Social from '../Social';
 import Footer from '../Footer';
 import HomeScreen from '../Screen/components/Home';
+import SubmitSessionScreen from '../Screen/components/SubmitSession';
 
 function App() {
   return (
-    <div id="app">
-      <header className="header header--page">
-        <CampInfo />
-        <Menu />
-      </header>
+    <Router>
+      <div id="app">
+        <header className="header header--page">
+          <CampInfo />
+          <Menu />
+        </header>
 
-      <HomeScreen />
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/submit-session" component={SubmitSessionScreen} />
 
-      <footer className="footer footer--page">
-        <Social />
-        <Footer />
-      </footer>
-    </div>
+        <footer className="footer footer--page">
+          <Social />
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
