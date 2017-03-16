f0590d4a223eb064898d0b8a4e59f1d49329c0ad
Brandon Williams <brandon@amazeelabs.com>
2017-03-15 23:46:44 -0500

    Scroll to top when "navigating" to submission form

--- src/components/Screen/components/SubmitSession/index.js
+++ src/components/Screen/components/SubmitSession/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Screen from '../../../Screen';
+import ScrollToTop from '../../../ScrollToTopOnMount';
 import SubmitSession from '../../../Frame/SubmitSession';
 
 function SubmitSessionScreen() {
   return (
     <Screen name="home">
+      <ScrollToTop />
       <SubmitSession />
     </Screen>
   );
