eee01755384c2975c4bac0cfd52f15883515d9a5
Maria Comas <unamaria@gmail.com>
2017-03-20 18:30:32 -0500

    Refactor Sponsors to get rid of missing key error.

--- src/components/Frame/Sponsors/Components/SponsorGroup/index.js
+++ src/components/Frame/Sponsors/Components/SponsorGroup/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import Sponsor from '../Sponsor';
 
-function Sponsor(props) {
+function SponsorGroup(props) {
   return (
     <section className={`sponsor-group sponsors--${props.name.toLowerCase()}`}>
       <div className="sponsor-group__title-wrapper">
         <h3>{props.name}</h3>
       </div>
       <div className="sponsor-group__sponsors-wrapper">
-        {props.sponsors}
+        {props.sponsors.map(sponsor =>
+          <Sponsor
+            key={sponsor.name}
+            name={sponsor.name}
+            link={sponsor.link}
+            logo={sponsor.logo}
+          />
+          )
+        }
       </div>
     </section>
   );
 }
 
-Sponsor.propTypes = {
+SponsorGroup.propTypes = {
   name: React.PropTypes.string.isRequired,
-  sponsors: React.PropTypes.array,
+  sponsors: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    link: React.PropTypes.string.isRequired,
+    logo: React.PropTypes.string.isRequired,
+  })).isRequired,
 };
 
-export default Sponsor;
+export default SponsorGroup;
