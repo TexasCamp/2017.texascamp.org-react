e412a33f75cccd115865e0a3977789ef9c856d7e
Brandon Williams <brandon@amazeelabs.com>
2017-04-11 18:38:11 +0200

    Made improvements to Session submissions
    * Add a loading message while form.io loads
    * Add a success message after form.io submission
    * Style 'add another' to look like a button
    * Don't hide the form on small screens

--- src/components/Frame/SubmitSession/index.js
+++ src/components/Frame/SubmitSession/index.js
@@ -9,17 +9,53 @@ import Title from '../../Title';
 import Content from '../../Content';
 import background from './sessions-bg.jpg';
 
-function Sessions() {
-  const classes = ['one-col', 'light-bg', 'fixed-bg'];
+class Sessions extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Page name="submit-session" background={background} classes={classes}>
-      <Title level="h2" title="Submit A Session" />
-      <Content classes={['content--body']}>
-        <Formio src="https://jqvzsivhnemmbtq.form.io/session" />
-      </Content>
-    </Page>
-  );
+    this.state = {
+      message: 'Loading...',
+      sessionSubmitted: false,
+    };
+
+    this.onLoad = this.onLoad.bind(this);
+    this.onSuccess = this.onSuccess.bind(this);
+  }
+
+  onLoad() {
+    this.setState({ message: '' });
+  }
+
+  onSuccess() {
+    this.setState({
+      sessionSubmitted: true,
+      message: 'Thanks for your submission, we\'ll holler at ya soon!',
+    });
+  }
+
+  render() {
+    const classes = ['one-col', 'light-bg', 'fixed-bg'];
+    let form = (<Formio
+      src="https://jqvzsivhnemmbtq.form.io/session"
+      onFormLoad={this.onLoad}
+      onFormSubmit={this.onSuccess}
+    />);
+
+    if (this.state.sessionSubmitted) {
+      form = <span />;
+    }
+
+    return (
+      <Page name="submit-session" background={background} classes={classes}>
+        <Title level="h2" title="Submit A Session" />
+        <Content classes={['content--body']}>
+          <div className="form-messages">{this.state.message}</div>
+          {form}
+        </Content>
+      </Page>
+    );
+  }
+
 }
 
 export default Sessions;
