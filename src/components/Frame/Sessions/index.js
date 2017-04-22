90706768933b6ad77649b14d4cc7b2326f630149
Brandon Williams <brandon@amazeelabs.com>
2017-04-22 19:02:50 -0400

    Add more session guideline information

--- src/components/Frame/Sessions/index.js
+++ src/components/Frame/Sessions/index.js
@@ -18,11 +18,20 @@ function Sessions() {
       </Link>
       <Content classes={['content--body']}>
         <p>
-          You have an idea?
-          We want to hear it.&nbsp;
-          <Link to="/submit-session">Submit your sessions here!</Link>
-          &nbsp;Session submissions close April 28th at midnight.
+          We are looking for folks just like you to speak to our frontier of Drupal enthusiasts!
+          Experienced speakers are always welcome, but camp is also a great place to start for
+          first-time speakers. We are looking for diverse speakers to present on a range of topics.
+          Whatever you are passionate about we are open to innovative ideas! It does not matter if
+          it is a session you have presented many times or a brand new idea you are just bubbling to
+          the surface. We want to hear from you!
         </p>
+        <p>
+          From the opening of session submission through the beginning of camp, we'll have
+          communication to assist speakers (new and old) in creating engaging and informative
+          sessions.
+        </p>
+        <p>Session submissions will close Friday, April 28, 2017.</p>
+        <p><Link to="/submit-session">Session Guidelines</Link></p>
       </Content>
     </Page>
   );
--- src/components/Frame/SubmitSession/index.js
+++ src/components/Frame/SubmitSession/index.js
@@ -49,6 +49,24 @@ class Sessions extends React.Component {
       <Page name="submit-session" background={background} classes={classes}>
         <Title level="h2" title="Submit A Session" />
         <Content classes={['content--body']}>
+          <p>
+            Texas Camp is soliciting sessions geared toward beginner through advanced Drupal users.
+            If you're passionate about the topic, we want to hear it! Subject matter can be Drupal,
+            PHP in general, related technologies, soft skills, testing, tools, workflow, etc.
+          </p>
+          <p>
+            We're looking for sessions that are 45 minutes long to allow 10 minutes for audience
+            Q+A. You may promote a project or organization you are working on if it is relevant, but
+            please no sales pitches. Speakers are expected to follow our code of conduct.
+          </p>
+          <p>
+            Speakers will receive a free ticket to the event. We are not yet able to pay speakers
+            for their time.
+          </p>
+          <p>
+            Email <a href="mailto:hello@texascamp.org">hello@texascamp.org</a> or tweet
+            &nbsp;<a href="https://twitter.com/drupaltexascamp">@drupaltexascamp</a> with questions.
+          </p>
           <div className="form-messages">{this.state.message}</div>
           {form}
         </Content>
