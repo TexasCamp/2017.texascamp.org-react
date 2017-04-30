3619b77900442c6c9b4c2b4ac150c655569455cd
Brandon Williams <brandon@amazeelabs.com>
2017-04-30 11:28:02 -0500

    Proof of concept example for getting data from our graphql proof of concept backend

--- src/components/Frame/About/index.js
+++ src/components/Frame/About/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
 import Page from '../../Page';
 import Title from '../../Title';
 import Content from '../../Content';
 import background from './about-bg.jpg';
 
-function About() {
+function About({ data: { sectionByName } }) {
   return (
     <Page name="about" background={background} classes={['two-col']}>
-      <Title level="h2" title="Three days in a two-step town" />
+      <Title level="h2" title={sectionByName ? sectionByName.title : ''} />
       <Content classes={['content--body']}>
         <p>
           Texas Camp is a homegrown event with the purpose of bringing the
@@ -20,4 +21,16 @@ function About() {
   );
 }
 
-export default About;
+About.propTypes = {
+  data: React.PropTypes.object,
+};
+
+export default graphql(gql`
+  query
+  root {
+    sectionByName(name:"about") {
+      title
+    }
+  }
+
+`)(About);
