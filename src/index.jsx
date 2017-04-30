ef9ca59ac8a4e07879fc38844d8d54a70597e0a8
Brandon Williams <brandon@amazeelabs.com>
2017-04-30 11:23:47 -0500

    Setup a graphql connection and make apollo available to all React components

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloProvider, ApolloClient, createNetworkInterface } from 'react-apollo';
 import App from './components/App';
 import './main.scss';
 
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://2017.texascamp.org.develop.us2.compact.amazee.io/graphql',
+  }),
+});
+
 render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
