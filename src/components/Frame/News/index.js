5f0d60ce3c972ef8ac92d0ac5208a0438b939c94
Brandon Williams <brandon@amazeelabs.com>
2017-05-13 16:32:09 -0500

    Update news section

--- src/components/Frame/News/index.js
+++ src/components/Frame/News/index.js
@@ -9,13 +9,15 @@ import ponyExpress from './pony-express.png';
 function News() {
   return (
     <Page name="news" background={background} classes={['two-col', 'light-bg']}>
-      <Title level="h2" title="Submission deadline fast approaching" />
+      <Title level="h2" title="You heard it here first" />
       <Content classes={['content--aside']}>
         <img src={ponyExpress} alt="sign up for news" width="376" />
         <NewsletterForm />
       </Content>
       <Content classes={['content--body']}>
-        <p>Visit this page to keep up with Texas Camp-related announcements.</p>
+        <a className="twitter-timeline" href="https://twitter.com/DrupalTexasCamp" data-height="500" data-dnt="true" data-link-color="#ebc387">
+          Tweets by DrupalTexasCamp
+        </a>
       </Content>
     </Page>
   );
