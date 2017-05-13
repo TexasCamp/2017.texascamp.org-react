7dcbd24ae66b4954f910ebaba214fb6d62c8d838
Brandon Williams <brandon@amazeelabs.com>
2017-05-13 15:40:57 -0500

    Update the session section

--- src/components/Frame/Sessions/index.js
+++ src/components/Frame/Sessions/index.js
@@ -6,30 +6,29 @@ import background from './sessions-bg.jpg';
 import wanted from './wanted-poster.png';
 
 function Sessions() {
+  const scheduleLink = 'https://docs.google.com/spreadsheets/d/1qb1K8GaPbmaXYMn7mA_M0Cvy9-CqV1MYzf14gL-ZIEs/edit#gid=834480689';
   return (
     <Page name="sessions" background={background} classes={['two-col', 'light-bg']}>
-      <Title level="h2" title="Step right up" />
+      <Title level="h2" title="A Mighty Fine Lineup" />
       <Content classes={['content--aside']}>
         <img src={wanted} alt="submit a session" width="411" />
       </Content>
       <Content classes={['content--body']}>
         <p>
-          We are looking for folks just like you to speak to our frontier of Drupal enthusiasts!
-          Experienced speakers are always welcome, but camp is also a great place to start for
-          first-time speakers. We are looking for diverse speakers to present on a range of topics.
-          Whatever you are passionate about we are open to innovative ideas! It does not matter if
-          it is a session you have presented many times or a brand new idea you are just bubbling to
-          the surface. We want to hear from you!
+          We’ve filled our&nbsp;
+          <a href={scheduleLink}>
+            schedule
+          </a> with a stellar lineup of Drupal experts
+          covering the latest and greatest in the industry. Our sessions range
+          from beginner to advanced so there’s something for everyone.
         </p>
         <p>
-          From the opening of session submission through the beginning of camp, we'll have
-          communication to assist speakers (new and old) in creating engaging and informative
-          sessions.
+          Texas Camp also offers Training, Sprints, and BOFs (Birds of a Feather
+          peer discussions) for those looking for something a little different.
         </p>
-        <p>Session submissions will close at midnight Monday, May 1, 2017.</p>
         <p>
-          <a href="https://docs.google.com/spreadsheets/d/1qb1K8GaPbmaXYMn7mA_M0Cvy9-CqV1MYzf14gL-ZIEs/edit#gid=0">
-            Submitted Sessions
+          <a href={scheduleLink}>
+            View the 2017 Texas Camp schedule.
           </a>
         </p>
       </Content>
